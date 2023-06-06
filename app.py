997afa27d34f98684201b61a2b8e3b01609806cc
Shoaib Ayaz <shoaib.botsify@gmail.com>
2023-06-07 00:20:01 +0500

    plus factor api added3

--- app.py
+++ app.py
@@ -22,26 +22,6 @@ def hook():
         ), 401)
 
 
-# Provide all products having the “+” (plus) symbol in the SKU field
-@app.route('/api/plusfactor/')
-def plus_factor():
-    db = Database()
-    data = db.getProductDetails("+")
-    if data:
-        db.closeConnection()
-        return make_response(jsonify(
-            success="YES",
-            status_code="201 OK",
-            body=data
-        ), 200)
-
-    return make_response(jsonify(
-        success="NO",
-        status_code="202 OK",
-        body='No product(s) here'
-    ), 202)
-
-
 # Provide product information given SKU
 @app.route('/api/productref/<sku>')
 def product_info(sku):
@@ -116,11 +96,6 @@ def ynlag_update(sku):
 # Provide all products with N Flag
 @app.route('/api/nflags')
 def nflag_list():
-    return make_response(jsonify(
-            success="YES",
-            status_code="201 OK",
-            body="HELLO"
-        ), 200)
     db = Database()
     data = db.getFlagStatusProducts('N')
     if data:
@@ -177,6 +152,25 @@ def category_listing():
         body='No Category(ies) here'
     ), 202)
 
+
+# Provide all products having the “+” (plus) symbol in the SKU field
+@app.route('/api/plusfactor/')
+def plusfactor_products():
+    db = Database()
+    data = db.getProductDetails("+")
+    if data:
+        db.closeConnection()
+        return make_response(jsonify(
+            success="YES",
+            status_code="201 OK",
+            body=data
+        ), 200)
+
+    return make_response(jsonify(
+        success="NO",
+        status_code="202 OK",
+        body='No product(s) here'
+    ), 202)
 
 
 if __name__ == "__main__":
